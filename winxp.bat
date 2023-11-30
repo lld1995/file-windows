e24183593708e80a604d0a789ab30141c3e938d3
lidong.lv <lidong.lv@westsnow.cn>
2023-11-30 14:58:00 +0800

    support winxp

---
+++ winxp.bat
@@ -0,0 +1,2 @@
+MinimumRequiredVersionHelper.exe out\build\x86-Debug\file.exe /MinimumRequiredVersion:5.1
+MinimumRequiredVersionHelper.exe out\build\x86-Release\file.exe /MinimumRequiredVersion:5.1
